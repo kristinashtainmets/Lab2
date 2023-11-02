a595d775d34a99a3739d0ba961e59e81fdc449fa
Кристина <kristina.shtaynmets@mail.ru>
2023-11-02 22:18:41 +0700

    Представление на заявки

--- designpro/design/views.py
+++ designpro/design/views.py
@@ -1,37 +1,18 @@
-from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+from .models import Application
 
 
-from .forms import CustomUserCreationForm
-
-
-# Эта функция будет отвечать за отображение начальной страницы
 def index(request):
-    # Здесь мы используем функцию render, чтобы отобразить наш шаблон index.html
-    return render(request, 'index.html')
+    Applications_title = Application.objects.all()
+    Applications_description = Application.description
+    Applications_category = Application.category
+    return render(request, 'index.html', context={Applications_title: Application.objects.all(),
+                                                  Applications_description: Application.description,
+                                                  Applications_category: Application.category})
 
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)  # Используйте свою собственную форму регистрации
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()  # Используйте свою собственную форму регистрации
-    return render(request, 'registration/register.html', {'form': form})
-
-
-def logout_view(request):
-    logout(request)
-    return redirect('/')
-
-from django.shortcuts import render
-from .models import DesignRequest
-
-def home(request):
-    completed_designs = DesignRequest.objects.filter(status='C').order_by('-timestamp')[:4]
-    in_progress_count = DesignRequest.objects.filter(status='P').count()
-    return render(request, 'index.html', {'completed_designs': completed_designs, 'in_progress_count': in_progress_count})
+class ApplicationListView(LoginRequiredMixin, generic.ListView):
+    model = Application
 
