f2d24a87dedb39284b11e4b6f7e2dff16b407e46
Кристина <kristina.shtaynmets@mail.ru>
2023-11-02 22:33:36 +0700

    Дополняем файл с моделями

--- designpro/design/models.py
+++ designpro/design/models.py
@@ -23,3 +23,34 @@ class CustomUser(AbstractUser):
         related_query_name="user",
     )
 
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+def get_name_file(instance, filename):
+    return '/'.join([get_random_string(length=5) + '_' + filename])
+
+
+class Application(models.Model):
+    STATUS_CHOICES = [
+        ('N', 'Новая'),
+        ('P', 'Принято в работу'),
+        ('C', 'Выполнено'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000, help_text="Enter a brief description of the application")
+    category = models.ManyToManyField(Category, help_text="Select a genre for this application")
+    photo_file = models.ImageField(max_length=254,upload_to='image/',
+                                   validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', 'bmp'])])
+    status = models.CharField(max_length=254, verbose_name='Статус', choices=STATUS_CHOICES, default='N')
+    date = models.DateTimeField(verbose_name='Дата добавления', auto_now_add=True)
+    user = models.ForeignKey(CustomUser, verbose_name='Пользователь', on_delete=models.CASCADE)
+
