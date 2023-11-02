d793cea54c59d5d4052a11bd7f824c86b463595c
Кристина <kristina.shtaynmets@mail.ru>
2023-11-02 22:10:23 +0700

    Добавляем в файл urls

--- designpro/design/urls.py
+++ designpro/design/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.register, name='register'),  # новый URL-маршрут для регистрации
-    # Это означает, что когда пользователь переходит на страницу 'design/', Django будет использовать функцию index
-    path('logout/', views.logout_view, name='logout'),  # новый URL-маршрут для выхода
-]
+
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),]
