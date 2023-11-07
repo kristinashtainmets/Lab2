1dce5e4de47e786ff02577432b9fac14ca18590e
Кристина <kristina.shtaynmets@mail.ru>
2023-11-07 09:57:24 +0700

    Добавляем ссылку на удаление категории

--- designpro/design/urls.py
+++ designpro/design/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import AdminDashboardView, CategoryCreateView
+from .views import AdminDashboardView, CategoryCreateView, CategoryDeleteView
 from .views import ProfileView, CreateRequestView, \
  DeleteRequestView
 
@@ -17,4 +17,6 @@ urlpatterns = [
                   path('delete_request/<int:pk>/', DeleteRequestView.as_view(), name='delete_request'),
                   path('admin_dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),
                   path('category/new/', CategoryCreateView.as_view(), name='category_new'),
+                  path('category/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
